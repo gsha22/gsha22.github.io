a9ab07d9f37ceefa6fd48b9038a4d5883ca4e095
Gabriel Sha <gsha@andrew.cmu.edu>
2025-01-09 00:41:23 -0500

    main page finished part 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,24 @@ import HomePage from "./components/HomePage";
 
 const HomeSection = () => {
   return (
-    <div className="relative flex flex-col w-full min-h-screen">
-      {/* Navbar */}
-      {/* <div className="relative z-50">
-        <Navbar />
-      </div> */}
+    <>
+    <head>
+      <title>Gabriel Sha</title>
+      <link rel="icon" href="g.svg"/>
+    </head>
+      <div className="relative flex flex-col w-full min-h-screen">
+        {/* Navbar */}
+        {/* <div className="relative z-50">
+          <Navbar />
+        </div> */}
 
-      {/* HomePageText */}
-      <div className="relative z-10">
-        <HomePage />
+        {/* HomePageText */}
+        <div className="relative z-10">
+          <HomePage />
+        </div>
       </div>
-    </div>
+    </>
+    
   );
 };
 
